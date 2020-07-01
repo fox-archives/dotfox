ebb0b0ab22add34a57a0a567f465197be1dce504
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-30 18:01:47 -0700

    feat: only process files according to their 'For' property

--- helper.go
+++ helper.go
@@ -8,7 +8,9 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 
+	"github.com/gobwas/glob"
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
@@ -26,10 +28,7 @@ func GetAbsolutePaths(relativePath string) struct {
 	SrcPath  string
 	DestPath string
 } {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
+	dir := getProjectDir()
 	destPath := path.Join(dir, relativePath)
 
 	srcPath := path.Join(_dirname(), "files", relativePath)
@@ -68,6 +67,67 @@ func ShouldRemoveExistingFile(path string, relativePath string, destContents []b
 	}
 }
 
+func getProjectDir() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error when trying to get the working directory")
+		panic(err)
+	}
+	return wd
+}
+
+func getProjectFiles() []string {
+	projectDir := getProjectDir()
+
+	matches, err := filepath.Glob(projectDir + "/*")
+	if err != nil {
+		panic(err)
+	}
+
+	matches2, err := filepath.Glob(projectDir + "/**")
+	if err != nil {
+		panic(err)
+	}
+
+	return append(matches, matches2...)
+}
+
+func projectFilesContain(glob glob.Glob) bool {
+	files := getProjectFiles()
+	var doesContain bool
+	for _, file := range files {
+		if glob.Match(file) {
+			doesContain = true
+			break
+		}
+	}
+
+	debug("Does project contain pattern %s?\n", glob)
+	return doesContain
+}
+
+func isFileRelevant(file File) bool {
+	projectContainsGoFiles := func() bool {
+		if projectFilesContain(glob.MustCompile("*.go")) {
+			return true
+		}
+		return false
+
+	}
+	switch file.For {
+	case "all":
+		return true
+	case "golang":
+		if projectContainsGoFiles() {
+			return true
+		}
+		return false
+	}
+
+	debug("File '%s' does not match case statement. Has value %s. Skipping\n", file.Path, file.For)
+	return false
+}
+
 func CopyFile(file File) {
 	abs := GetAbsolutePaths(file.Path)
 	srcFile := abs.SrcPath
@@ -81,6 +141,15 @@ func CopyFile(file File) {
 	srcContents, err := ioutil.ReadFile(srcFile)
 	if err != nil {
 		panic(err)
+	}
+
+	// check to see if we should even be trying to copy the file
+	// over. for example scripts/go.sh should only be copied when
+	// there are .go files in the repository
+	isFileRelevant := isFileRelevant(file)
+	if !isFileRelevant {
+		printInfo("Non-relevant file '%s' is being skipped\n", file.Path)
+		return
 	}
 
 	// prompt to remove preexisting file if it exists
