aeeac6e746a97ef93f7384522dc42d00c52abebf
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-12-25 13:55:24 -0800

    update code

--- README.md
+++ README.md
@@ -9,6 +9,6 @@
 - BROKEN_S: symlink exists, but points to wrong location
 - ROGUE_F: file exists in location
 - ROGUE_D: dir exists in location
-- TODO_F: file does not exist in location. must todo copy it in there
-- TODO_D: same for dir
+- TOLINK_F: file does not exist in location. will be linked during reconciliation
+- TOLINK_D: same for dir
 - MISSING: file missing from both src and dest
--- src/dotty.nim
+++ src/dotty.nim
@@ -16,10 +16,84 @@ proc xdgData: string =
    else:
       return joinPath(getEnv("HOME"), ".local/share")
 
-let home = getEnv("HOME") & "/"
+# remove trailing slash
+proc rts(str: string): string =
+   if endsWith(str, '/'):
+      return str[0 .. ^2]
+   return str
+
+proc symlinkResolvedProperly(src: string, dest: string, symlink: string): bool =
+   let rel = symlink[len(dest) .. ^1]
+
+   if rts(expandSymlink(symlink)) == joinPath(src, rel):
+      return true
+   else:
+      return false
+
+# checks to see if src and dest are both a folder or file. if not, aborts
+proc isBothFileOrFolder(src: string, dest: string, rel: string): bool =
+   let pathOne = joinPath(src, rel)
+   let pathTwo = joinPath(dest, rel)
+
+   if dirExists(pathOne) and dirExists(pathTwo):
+      return true
+   elif fileExists(pathOne) and fileExists(pathTwo):
+      return true
+   else:
+      return false
+
+proc status(src: string, dest: string, dotConfigFiles: seq[string]) =
+   for i, file in dotConfigFiles:
+      if symlinkExists(file):
+         if symlinkResolvedProperly(src, dest, file):
+            # symlinks pointing to a file or folder may or may not have a trailing slash. skip ones that do for consistency
+            if endsWith(expandSymlink(file), '/'):
+               echo "[OK_SLASH] " & file
+            else:
+               echo "[OK]       " & file
+         else:
+            echo "[BROKEN_S] " & file
+            echo "          -> " & expandSymlink(file)
+      elif fileExists(file):
+         echo "[ROGUE_F]  " & file
+      elif dirExists(file):
+         echo "[ROGUE_D]  " & file
+      # nothing exists, we create it
+      else:
+         let rel = file[len(dest) .. ^1]
+         if fileExists(joinPath(src, rel)):
+            echo "[TOLINK_F] " & file
+         elif dirExists(joinPath(src, rel)):
+            echo "[TOLINK_D] " & file
+         else:
+            echo "[MISSING]  " & file
+
+proc reconcile(src: string, dest: string, dotConfigFiles: seq[string]) =
+   for i, file in dotConfigFiles:
+      if symlinkExists(file):
+         if symlinkResolvedProperly(src, dest, file):
+            # everything OK except the trailing slash; fix it
+            if endsWith(expandSymlink(file), '/'):
+               echo  "FIX OK_SLASH: " & file
+               let temp = expandSymlink(file)
+               removeFile(file)
+               createSymlink(rts(temp), file)
+         else:
+            echo "FIX BROKEN_S: " & file
+            let temp = expandSymlink(file)
+            removeFile(file)
+            createSymlink(rts(temp), file)
+      elif fileExists(file):
+         echo "[ROGUE_F]  " & file
+      elif dirExists(file):
+         echo "[ROGUE_D]  " & file
+      else:
+         let rel = file[len(dest) .. ^1]
+         createSymlink(joinPath(src, rel), file)
+
+let home = getHomeDir() & "/"
 let cfg = xdgCfg() & "/"
 let data = xdgData() & "/"
-
 let dotConfigFiles = [
    home & ".alsoftrc",
    home & ".bash_logout",
@@ -103,80 +177,10 @@ let dotConfigFiles = [
    data & "applications/Zettlr.desktop"
 ]
 
-# remove trailing slash
-proc rts(str: string): string =
-   if endsWith(str, '/'):
-      return str[0 .. ^2]
-   return str
+let dest = getHomeDir()
+let src = joinPath(getHomeDir(), ".dots/user")
 
+status(src, dest, @dotConfigFiles)
+# reconcile(srf, dest, @dotConfigFiles)
 
-
-proc symlinkResolvedProperly(src: string, dest: string, symlink: string): bool =
-   let rel = symlink[len(dest) .. ^1]
-
-   if rts(expandSymlink(symlink)) == joinPath(src, rel):
-      return true
-   else:
-      return false
-
-# checks to see if src and dest are both a folder or file. if not, aborts
-proc isBothFileOrFolder(src: string, dest: string, rel: string): bool =
-   let pathOne = joinPath(src, rel)
-   let pathTwo = joinPath(dest, rel)
-
-   if dirExists(pathOne) and dirExists(pathTwo):
-      return true
-   elif fileExists(pathOne) and fileExists(pathTwo):
-      return true
-   else:
-      return false
-
-
-proc status(src: string, dest: string) =
-   for i, file in dotConfigFiles:
-      if symlinkExists(file):
-         if symlinkResolvedProperly(src, dest, file):
-            # symlinks pointing to a file or folder may or may not have a trailing slash. skip ones that do for consistency
-            if endsWith(expandSymlink(file), '/'):
-               echo "[OK_SLASH] " & file
-            else:
-               echo "[OK]       " & file
-         else:
-            echo "[BROKEN_S] " & file
-            echo "          -> " & expandSymlink(file)
-      elif fileExists(file):
-         echo "[ROGUE_F]  " & file
-      elif dirExists(file):
-         echo "[ROGUE_D]  " & file
-      # nothing exists, we create it
-      else:
-         let rel = file[len(dest) .. ^1]
-         if fileExists(joinPath(src, rel)):
-            echo "[TOLINK_F] " & file
-         elif dirExists(joinPath(src, rel)):
-            echo "[TOLINK_D] " & file
-         else:
-            echo "[MISSING]  " & file
-
-proc reconcile(src: string, dest: string) =
-   for i, file in dotConfigFiles:
-      if symlinkExists(file):
-         if symlinkResolvedProperly(src, dest, file):
-            # everything OK except the trailing slash; fix it
-            if endsWith(expandSymlink(file), '/'):
-               let temp = expandSymlink(file)
-               removeFile(file)
-               createSymlink(rts(temp), file)
-         else:
-            let temp = expandSymlink(file)
-            removeFile(file)
-            createSymlink(rts(temp), file)
-      else:
-         let rel = file[len(dest) .. ^1]
-         createSymlink(joinPath(src, rel), file)
-
-let dest = getEnv("HOME")
-let src = joinPath(getEnv("HOME"), ".dots/user")
-
-status(src, dest)
-# reconcile(srf, dest)
+echo len(walkDir('.'))
